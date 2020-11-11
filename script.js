3666db36b1e29bf2c2031e3d7863f99df5f98ac0
DTrombla <duncan@trombla.org>
2020-11-11 12:04:42 -0800

    added token and api stuff

---
+++ script.js
@@ -0,0 +1,50 @@
+var type = "animals"
+//checks if token is nonexistant or if it is expired, if so it generates a new one
+function checkToken(){
+  var token = JSON.parse(localStorage.getItem("token"));
+  if(localStorage.getItem("token") == null ||  Date.now() > token.expiry + 3600000){
+    getToken();
+  };
+};
+//retrieves token from api
+function getToken(){
+  var queryURL = "https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/oauth2/token";
+  $.ajax({
+  header: origin,
+  url: queryURL,
+  method: "POST",
+  data:{"grant_type":"client_credentials",
+        "client_id":"C38uh1DS4F1g75CUZCu8m7iq2hTY5358shsEX4IttueHZjaDMt",
+        "client_secret":"AUqzbULUZJs5KL1j6T6iEoRxwIqNuyH3UMXCqvYU"
+        }   
+  }).then(function(response) {
+    var now = Date.now();
+    var token = {
+      value: response.access_token,
+      expiry: now
+    }
+    localStorage.setItem("token", JSON.stringify(token))
+  })
+};
+
+// function to run when searching
+function search(){
+  var token = JSON.parse(localStorage.getItem("token"))
+  var queryURL = "https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/" + type;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    headers:{"Content-Type": "application/json","Authorization":"Bearer " + token.value}
+  }).then(function(response){
+    console.log(response);
+  });
+};
+
+//runs when page opens or reloads
+checkToken();
+
+//runs when search button is pressed 
+$(".my-sm-0").on("click", function(event){
+    event.preventDefault();
+    search();
+});
